effe0bf66e99ffbbe08b5fcd976d54bf2d143fb0
CMRoss <ChrisRoss@u.boisestate.edu>
2021-01-16 13:35:23 -0700

    myChanges

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,54 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          title: Text('Ask Me Anything'),
+          backgroundColor: Colors.blue.shade800,
+        ),
+        body: Ball());
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int eightBallAns = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+                onPressed: () {
+                  roll8Ball();
+                  print('Eight Ball has been awakened!!');
+                },
+                child: Image.asset('images/ball$eightBallAns.png')),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void roll8Ball() {
+    setState(() {
+      eightBallAns = Random().nextInt(4) + 1;
+    });
+  }
+}
